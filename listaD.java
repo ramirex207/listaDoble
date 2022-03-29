6c2201c80efdb763960f4d4b5c08498858a01257
ramirex207 <ramiro.cuevas.choque@outlook.com>
2022-03-29 18:17:28 -0400

    [add] funcionando :)

--- listaD.java
+++ listaD.java
@@ -1,5 +1,56 @@
 package listaDoble;
+import javax.swing.JOptionPane;
 
 public class listaD {
-
+	private nodoD inicio,fin;
+	//constructor por defecto
+	public listaD() {
+		inicio=fin=null;
+	}
+	//constructor ordinario
+	public boolean estaVacia() {
+		return inicio==null;
+	}
+	//metodo para agregar al final
+	public void agregarFinal(int dato) {
+		if (!estaVacia()) {
+			fin= new nodoD(dato,null,fin);
+			fin.ant.sig=fin;
+		}else {
+			inicio=fin=new nodoD(dato);
+		}
+	}
+	//metodo para agregar al inicio
+	public void agregarInicio(int dato) {
+		if (!estaVacia()) {
+			inicio= new nodoD(dato,inicio,null);
+			inicio.sig.ant=inicio;
+		}else {
+			inicio=fin=new nodoD(dato);
+		}
+	} 
+	//metodo para mostrar lista de inicio a fin 
+	public void mostrarListaInicioFin() {
+		if (!estaVacia()) {
+			String datos="<=>";
+			nodoD aux = inicio;
+			while(aux!=null) {
+				datos = datos + "["+aux.dato+"]<=>";
+				aux=aux.sig;
+			}
+			System.out.println("mostrando lista de inicio a fin"+datos);			
+		}
+	}
+	//metodo para mostrar la lista de fin a inicio
+	public void mostrarListaFinInicio() {
+		if (!estaVacia()) {
+			String datos="<=>";
+			nodoD aux = fin;
+			while(aux!=null) {
+				datos = datos + "["+aux.dato+"]<=>";
+				aux=aux.ant;
+			}
+			System.out.println("mostrando lista de fin a inicio "+datos);
+		}
+	}
 }
--- principal.java
+++ principal.java
@@ -1,10 +1,54 @@
 package listaDoble;
+import java.util.Scanner;
 
 public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		listaD lisD= new listaD();
+		int dato;
+		Scanner leer = new Scanner(System.in);
+		int op;
+		do {
+        System.out.println("****************OPERACIONES CON UNA LISTA ENLAZADA**************************\n\n");
+        System.out.println("**********************************************************");
+        System.out.println("1. agregar nodo al inicio");
+        System.out.println("2. agregar nodo al final");
+        System.out.println("3. mostrar lista de inicio a fin");
+        System.out.println("4. mostrar lista de fin a inicio");
+        System.out.println("0. Salir");
+        System.out.println("***********************************************************");
+        op = leer.nextInt();
+	        switch(op) {
+			case 1:
+				 System.out.println("ingresa un numero");
+				 dato=leer.nextInt();
+				 lisD.agregarInicio(dato);
+				break; 
+			case 2:	
+				System.out.println("ingresa un numero");
+				dato=leer.nextInt();
+				lisD.agregarFinal(dato);
+				break;
+			case 3:
+				if(!lisD.estaVacia()) {
+					lisD.mostrarListaInicioFin();
+				}
+				else {
+					System.out.println("la lista esta vacia");	
+				}
+				break;
+			case 4:
+				if(!lisD.estaVacia()) {
+					lisD.mostrarListaFinInicio();
+				}
+				else {
+					System.out.println("la lista esta vacia");	
+				}
+				break;
+	        }
+	        
+		}while (op!=0); 
 	}
 
 }
